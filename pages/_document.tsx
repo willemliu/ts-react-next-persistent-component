96266fe29d37626be3aefd510e37bdf0f668cb39
Willem Liu <willem_liu@hotmail.com>
2019-05-01 10:52:27 +0200

    Added meta description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,7 @@ export default class MyDocument extends Document {
             <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1,user-scalable=no"/>
             <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+            <meta name="description" content="This is an example project showcasing the use of Next.JS to create a persistant player which survives navigation"/>
         </Head>
         <body>
             <Main />
