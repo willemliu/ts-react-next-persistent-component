0da5940fa681661f7db61dcba9e2e747d356f8d8
Willem Liu <willem_liu@hotmail.com>
2019-06-11 13:27:03 +0200

    Changed some paths

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,9 +11,9 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+            <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1,user-scalable=no"/>
-            <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
             <meta name="description" content="This is an example project showcasing the use of Next.JS to create a persistant player which survives navigation"/>
             <link rel="manifest" href="/static/manifest.json" />
             <link rel="icon" type="image/ico" href="/static/favicon.ico"/>
