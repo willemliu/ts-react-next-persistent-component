990bef76d9a098f358ff8f6c9aff199cdb9d0811
Willem Liu <willem_liu@hotmail.com>
2019-06-11 12:35:20 +0200

    PWA config

--- next.config.js
+++ next.config.js
@@ -4,5 +4,23 @@ const {PHASE_DEVELOPMENT_SERVER} = require("next/constants");
 
 module.exports = withTypescript(withOffline({
     target: process.env.NOW_SERVERLESS === 'false' ? 'server' : 'serverless',
-    pageExtensions: ["jsx", "js", "ts", "tsx"]
+    pageExtensions: ["jsx", "js", "ts", "tsx"],
+    workboxOpts: {
+        swDest: 'static/service-worker.js',
+        runtimeCaching: [{
+            urlPattern: /^https?.*/,
+            handler: 'NetworkFirst',
+            options: {
+                cacheName: 'https-calls',
+                networkTimeoutSeconds: 15,
+                expiration: {
+                    maxEntries: 150,
+                    maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+                },
+                cacheableResponse: {
+                    statuses: [0, 200],
+                },
+            },
+        }]
+    }
 }));
