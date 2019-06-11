afce5153ab0af3a2be58465918282ba537d33578
Willem Liu <willem_liu@hotmail.com>
2019-06-11 11:31:25 +0200

    Fixed build for Azure

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ const withTypescript = require("@zeit/next-typescript");
 const {PHASE_DEVELOPMENT_SERVER} = require("next/constants");
 
 module.exports = withTypescript({
-    target: 'serverless',
+    target: process.env.NOW_SERVERLESS === 'false' ? 'server' : 'serverless',
     pageExtensions: ["jsx", "js", "ts", "tsx"]
 });
