bbeacfb0ace3fb424520d15ff307aa2355f253a4
Willem Liu <willem_liu@hotmail.com>
2019-06-11 12:21:11 +0200

    Added next-offline

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
+const withOffline = require('next-offline');
 const withTypescript = require("@zeit/next-typescript");
 const {PHASE_DEVELOPMENT_SERVER} = require("next/constants");
 
-module.exports = withTypescript({
+module.exports = withTypescript(withOffline({
     target: process.env.NOW_SERVERLESS === 'false' ? 'server' : 'serverless',
     pageExtensions: ["jsx", "js", "ts", "tsx"]
-});
+}));
