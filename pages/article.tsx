16f397ce0fe25120efc041fa4534e4ff8801f986
Willem Liu <willem_liu@hotmail.com>
2019-06-12 11:38:58 +0200

    Testing

--- pages/article.tsx
+++ pages/article.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Article(props: any) {
     return (
         <>
-            <h1>Article page</h1>
+            <h1>Article page develop</h1>
             <p>ID: {props.articleId}</p>
         </>
     );
