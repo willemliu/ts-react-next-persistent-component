4fdc54549119976a5bf10c80607ca845f843048b
Willem Liu <willem_liu@hotmail.com>
2019-06-12 15:54:30 +0200

    Tryout sw push

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withOffline = require('next-offline');
 const withTypescript = require("@zeit/next-typescript");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const {PHASE_DEVELOPMENT_SERVER} = require("next/constants");
 const { join } = require('path');
 
@@ -22,6 +23,13 @@ module.exports = withTypescript(withOffline({
                     statuses: [0, 200],
                 },
             },
-        }]
+        }],
+        importScripts: ['/static/sw-push-listener.js']
+    },
+    webpack: (config) => {
+        // this will output your push listener file to .next folder
+        // check CopyWebpackPlugin docs if you want to change the destination (e.g. /static or /.next/static)
+        config.plugins.push(new CopyWebpackPlugin([{from: 'static/sw-push-listener.js', src: '.next/sw-push-listener.js'}]));
+        return config;
     }
 }));
---
+++ static/sw-push-listener.js
@@ -0,0 +1,41 @@
+console.log('sw-push-listener');
+
+self.addEventListener('push', function (event) {
+    if (!(self.Notification && self.Notification.permission === 'granted')) {
+        return;
+    }
+
+    const sendNotification = json => {
+        console.log(JSON.stringify(json, null, 2));
+        return self.registration.showNotification(json.title, json.options);
+    };
+
+    if (event.data) {
+        console.log(event.data);
+        const message = event.data.json();
+        event.waitUntil(sendNotification(message));
+    }
+});
+
+self.addEventListener('notificationclick', (event) => {
+  console.log('On notification click: ', event.notification.tag, event);
+  event.notification.close();
+
+    const url = event.notification.actions[0].action;
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(clients.matchAll({
+    type: "window"
+  }).then((clientList) => {
+    for (var i = 0; i < clientList.length; i++) {
+      var client = clientList[i];
+      if (client.url === url && 'focus' in client) {
+        return client.focus();
+      }
+    }
+    if (clients.openWindow) {
+      return clients.openWindow(url);
+    }
+  }));
+});
