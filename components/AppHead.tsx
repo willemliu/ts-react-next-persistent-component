9ee6215b4b04bb11a89d87f74cc44140896eb37d
Willem Liu <willem_liu@hotmail.com>
2019-06-14 17:41:39 +0200

    Allow user scalability

--- components/AppHead.tsx
+++ components/AppHead.tsx
@@ -20,7 +20,7 @@ function AppHead(props: any) {
     return (
         <Head>
             <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8"/>
-            {useAmp() ? null : <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1,user-scalable=1"/>}
+            {useAmp() ? null : <meta name="viewport" content="width=device-width, initial-scale=1,user-scalable=1"/>}
             <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
             <meta name="theme-color" content="#000000"/>
             <meta name="description" content="This is an example project showcasing the use of Next.JS to create a persistant player which survives navigation"/>
