99dfb159248f76d51c72d7cdc39f496b2b7ffd43
Willem Liu <willem_liu@hotmail.com>
2019-06-18 14:01:21 +0200

    Using tvmaze as data-source for teasers on section

--- pages/section.tsx
+++ pages/section.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 import { Log } from "../utils/log";
 import { getClient, getIsServer } from "../utils/app";
 
-export default function Section() {    
+export default function Section(props: any) {
     useEffect(() => {
         // Log.info({client: getClient(), view: 'section'});
         [].slice.call(document.querySelectorAll('[href]')).forEach((link: HTMLAnchorElement) => {
@@ -34,103 +34,87 @@ export default function Section() {
         <StyledSection>
             <GlobalStyle/>
             <main>
-                <OpeningTeaser
-                    id="1286450"
-                    baseUrl="https://fd.nl"
-                    url="/beurs/1286450/analisten-twijfelen-aan-renteverhoging-ecb"
-                    image={{
-                        src: 'https://images.fd.nl/-4mOGVA1ry0LHTzjvcxdyT-FH-E.jpg?fit=crop&crop=faces&auto=format&fm=jpg&q=45&w=599&h=337&rect=.0%2C.1557223264540338%2C.9999999999999999%2C.8442776735459663',
-                        alt: "Mario Draghi, president of the European Central Bank (ECB), departs a rates decision news conference in Frankfurt, Germany, on Thursday, Dec. 13, 2018. Draghi said risks to the euro-area economy are worsening even as he called time on the European Central Banks' flagship deflation-fighting tool. Photographer: Alex Kraus/Bloomberg"
-                    }}
-                    alt="Mario Draghi, president of the European Central Bank (ECB), departs a rates decision news conference in Frankfurt, Germany, on Thursday, Dec. 13, 2018. Draghi said risks to the euro-area economy are worsening even as he called time on the European Central Banks flagship deflation-fighting tool. Photographer: Alex Kraus/Bloomberg"
-                    sourceSets={[{
-                        media: "(max-width: 640px)",
-                        srcSet: `https://images.fd.nl/-4mOGVA1ry0LHTzjvcxdyT-FH-E.jpg?fit=crop&crop=faces&auto=format&fm=jpg&q=45&w=599&h=337&rect=.0%2C.1557223264540338%2C.9999999999999999%2C.8442776735459663`
-                    }, {
-                        media: "(max-width: 860px)",
-                        srcSet: `https://images.fd.nl/-4mOGVA1ry0LHTzjvcxdyT-FH-E.jpg?fit=crop&crop=faces&auto=format&fm=jpg&q=45&w=115&h=115&rect=.20625%2C.0%2C.66625%2C.9999999999999999`
-                    }, {
-                        media: "(min-width: 861px)",
-                        srcSet: `https://images.fd.nl/-4mOGVA1ry0LHTzjvcxdyT-FH-E.jpg?fit=crop&crop=faces&auto=format&fm=jpg&q=45&w=300&h=169&rect=.0%2C.1557223264540338%2C.9999999999999999%2C.8442776735459663`
-                    }]}
-                    figCaption="Mario Draghi, president of the European Central Bank (ECB), departs a rates decision news conference in Frankfurt, Germany, on Thursday, Dec. 13, 2018. Draghi said risks to the euro-area economy are worsening even as he called time on the European Central Banks' flagship deflation-fighting tool. Photographer: Alex Kraus/Bloomberg"
-                    subject="Monetair"
-                    readableAge="7 uur"
-                    date="17 Nov 1981"
-                    title="Analisten twijfelen aan renteverhoging ECB"
-                    description="Economische groei en de inflatie ontwikkelen zich niet zoals verwacht."
-                    related={[{
-                        longread: true,
-                        title: '‘Als vrouw moet je altijd onafhankelijk zijn’',
-                        url: '/profiel/1235280/als-vrouw-moet-je-onafhankelijk-zijn',
-                        uuid: "1235280"
-                    }, {
-                        title: 'President van de Amerikaanse Fed hamert op onafhankelijkheid',
-                        url: '/economie-politiek/1235274/president-van-de-amerikaanse-fed-hamert-op-onafhankelijkheid',
-                        uuid: "1235274"
-                    }]}
-                />
-                <Teaser
-                    id="123"
-                    title="Congo verwacht weinig van Nederlands ebola-vaccin"
-                    description="Autoriteiten in het Afrikaanse land vrezen toename van verzet en verwarring bij inzet van tweede vaccin, dat in Nederland wordt gemaakt."
-                    baseUrl=""
-                    url="/economie-politiek/1304718/congo-verwacht-weinig-van-nederlands-ebola-vaccin"
-                    readableAge="2 uur"
-                    date="13:11 13-06-2019"
-                    subject="buitenland"
-                    image={{
-                        src: 'https://images.fd.nl/BjzAQW5j11hip-ad0R6OxM0CJoA.jpg?fit=crop&crop=faces&auto=format&w=599&h=337&q=45&fm=jpg&cs=tinysrgb&rect=.0%2C.0787992495309568%2C.9999999999999999%2C.8442776735459663',
-                        alt: 'Congo verwacht weinig van Nederlands ebola-vaccin'
-                    }}
-                    sourceSets={[{
-                        media: '(max-width: 640px)',
-                        srcSet: `https://images.fd.nl/BjzAQW5j11hip-ad0R6OxM0CJoA.jpg?fit=crop&crop=faces&auto=format&w=599&h=337&q=45&fm=jpg&cs=tinysrgb&rect=.0%2C.0787992495309568%2C.9999999999999999%2C.8442776735459663`
-                    }, {
-                        media: '(max-width: 860px)',
-                        srcSet: `https://images.fd.nl/BjzAQW5j11hip-ad0R6OxM0CJoA.jpg?fit=crop&crop=faces&auto=format&w=115&h=115&q=45&fm=jpg&cs=tinysrgb&rect=.16125%2C.0%2C.66625%2C.9999999999999999`
-                    }, {
-                        media: '(min-width: 861px)',
-                        srcSet: `https://images.fd.nl/BjzAQW5j11hip-ad0R6OxM0CJoA.jpg?fit=crop&crop=faces&auto=format&w=115&h=115&q=45&fm=jpg&cs=tinysrgb&rect=.16125%2C.0%2C.66625%2C.9999999999999999`
-                    }]}
-                    related={[{
-                        longread: true,
-                        title: '‘Als vrouw moet je altijd onafhankelijk zijn’',
-                        url: '/profiel/1235280/als-vrouw-moet-je-onafhankelijk-zijn',
-                        uuid: "12345"
-                    }, {
-                        title: 'President van de Amerikaanse Fed hamert op onafhankelijkheid',
-                        url: '/economie-politiek/1235274/president-van-de-amerikaanse-fed-hamert-op-onafhankelijkheid',
-                        uuid: "123456"
-                    }]}    
-                />
+                {props.teasers.length ? (
+                    <OpeningTeaser
+                        key={props.teasers[0].show.id}
+                        id={props.teasers[0].show.id}
+                        baseUrl=""
+                        alt={props.teasers[0].show.name}
+                        title={props.teasers[0].show.name}
+                        description={props.teasers[0].show.summary}
+                        date={props.teasers[0].show.permiered}
+                        readableAge={props.teasers[0].show.runtime}
+                        subject={props.teasers[0].show.type}
+                        cardStyle={"default"}
+                        image={{src: props.teasers[0].show.image.medium, alt: props.teasers[0].show.name}}
+                        sourceSets={[{
+                            media: "(max-width: 640px)",
+                            srcSet: props.teasers[0].show.image.medium
+                        }, {
+                            media: "(max-width: 860px)",
+                            srcSet: props.teasers[0].show.image.medium
+                        }, {
+                            media: "(min-width: 861px)",
+                            srcSet: props.teasers[0].show.image.original
+                        }]}
+                        url={props.teasers[0].show.url.replace('http://www.tvmaze.com', '')}
+                    />) : null
+                }
+
+                {props.teasers.map((teaser: any, idx: number) => {
+                    if (idx === 0) return null;
+                    return <Teaser
+                        key={teaser.show.id}
+                        id={teaser.show.id}
+                        baseUrl=""
+                        title={teaser.show.name}
+                        description={teaser.show.summary}
+                        date={teaser.show.permiered}
+                        readableAge={teaser.show.runtime}
+                        subject={teaser.show.type}
+                        cardStyle={"default"}
+                        image={{src: teaser.show.image.medium, alt: teaser.show.name}}
+                        sourceSets={[{
+                            media: "(max-width: 640px)",
+                            srcSet: teaser.show.image.medium
+                        }, {
+                            media: "(max-width: 860px)",
+                            srcSet: teaser.show.image.medium
+                        }, {
+                            media: "(min-width: 861px)",
+                            srcSet: teaser.show.image.original
+                        }]}
+                        url={teaser.show.url.replace('http://www.tvmaze.com', '')}
+                    />
+                })}
             </main>
             <aside>
-                <SquareTeaser
-                    id="1292665"
-                    baseUrl="https://fd.nl"
-                    url="/futures/1292665/een-muur-bouwen-is-makkelijker-dan-robots-stukslaan"
-                    image={{
-                        src: 'https://images.fd.nl/nheGAUNYeo_AjGfa7ob4cqxoreI.jpg?fit=crop&crop=faces&auto=format&fm=jpg&q=45&w=599&h=337&rect=.0%2C.1557223264540338%2C.9999999999999999%2C.8442776735459663',
-                        alt: 'Amsterdam doet ertoe'
-                    }}
-                    sourceSets={[{
-                        media: "(max-width: 640px)",
-                        srcSet: `https://images.fd.nl/nheGAUNYeo_AjGfa7ob4cqxoreI.jpg?fit=crop&crop=faces&auto=format&fm=jpg&q=45&w=599&h=337&rect=.0%2C.1557223264540338%2C.9999999999999999%2C.8442776735459663`
-                    }, {
-                        media: "(max-width: 860px)",
-                        srcSet: `https://images.fd.nl/nheGAUNYeo_AjGfa7ob4cqxoreI.jpg?fit=crop&crop=faces&auto=format&fm=jpg&q=45&w=115&h=115&rect=.20625%2C.0%2C.66625%2C.9999999999999999`
-                    }, {
-                        media: "(min-width: 861px)",
-                        srcSet: `https://images.fd.nl/nheGAUNYeo_AjGfa7ob4cqxoreI.jpg?fit=crop&crop=faces&auto=format&fm=jpg&q=45&w=300&h=169&rect=.0%2C.1557223264540338%2C.9999999999999999%2C.8442776735459663`
-                    }]}
-                    figCaption="Francis Fukuyama: 'Een muur bouwen is makkelijker dan robots stukslaan'"
-                    subject="Samenleving"
-                    readableAge="6 uur"
-                    date="17 Nov 1981"
-                    title="Francis Fukuyama: 'Een muur bouwen is makkelijker dan robots stukslaan'"
-                    description="Identiteitspolitiek leidt zowel op links als op rechts tot uitwassen zoals de muur van Trump, zegt Francis Fukuyama. We kunnen ons beter druk maken over andere kwesties, zoals robotisering."
-                />
+                {props.teasers.map((teaser: any, idx: number) => {
+                    if (idx === 0) return null;
+                    return <SquareTeaser
+                        key={teaser.show.id}
+                        id={teaser.show.id}
+                        baseUrl=""
+                        title={teaser.show.name}
+                        description={teaser.show.summary}
+                        date={teaser.show.permiered}
+                        readableAge={teaser.show.runtime}
+                        subject={teaser.show.type}
+                        cardStyle={"default"}
+                        image={{src: teaser.show.image.medium, alt: teaser.show.name}}
+                        sourceSets={[{
+                            media: "(max-width: 640px)",
+                            srcSet: teaser.show.image.medium
+                        }, {
+                            media: "(max-width: 860px)",
+                            srcSet: teaser.show.image.medium
+                        }, {
+                            media: "(min-width: 861px)",
+                            srcSet: teaser.show.image.original
+                        }]}
+                        url={teaser.show.url.replace('http://www.tvmaze.com', '')}
+                    />
+                })}
 
                 <Link prefetch as="/beurs/1286450/analisten-twijfelen-aan-renteverhoging-ecb" href="/article?articleId=1286450">
                     <button>Article 1286450</button>
@@ -142,8 +126,10 @@ export default function Section() {
 }
 
 Section.getInitialProps = async () => {
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+    const teasers = await res.json();
     Log.info({client: getClient(), view: 'section', server: getIsServer()});
-    return {};
+    return {teasers};
 };
 
 const GlobalStyle = createGlobalStyle`
