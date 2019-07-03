90743fbac195fa374101982a3446d9158442a5c5
Willem Liu <willem_liu@hotmail.com>
2019-07-03 16:08:36 +0200

    Added webpack bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,38 +1,57 @@
-const withOffline = require('next-offline');
+const withOffline = require("next-offline");
 const withTypescript = require("@zeit/next-typescript");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const {PHASE_DEVELOPMENT_SERVER} = require("next/constants");
-const { join } = require('path');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: process.env.WEBPACK_BUNDLE_ANALYZER === "true"
+});
+const { join } = require("path");
 
-module.exports = withTypescript(withOffline({
-    target: process.env.NOW_SERVERLESS === 'false' ? 'server' : 'serverless',
-    pageExtensions: ["jsx", "js", "ts", "tsx"],
-    workboxOpts: {
-        swDest: 'static/service-worker.js',
-        runtimeCaching: [{
-            urlPattern: /^https?.*\.[a-zA-Z0-9]*$/,
-            handler: 'NetworkFirst',
-            options: {
-                cacheName: 'https-calls',
-                networkTimeoutSeconds: 15,
-                expiration: {
-                    maxEntries: 150,
-                    maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
-                },
-                cacheableResponse: {
-                    statuses: [0, 200],
-                },
+module.exports = withTypescript(
+    withBundleAnalyzer(
+        withOffline({
+            target:
+                process.env.NOW_SERVERLESS === "false"
+                    ? "server"
+                    : "serverless",
+            pageExtensions: ["jsx", "js", "ts", "tsx"],
+            workboxOpts: {
+                swDest: "static/service-worker.js",
+                runtimeCaching: [
+                    {
+                        urlPattern: /^https?.*\.[a-zA-Z0-9]*$/,
+                        handler: "NetworkFirst",
+                        options: {
+                            cacheName: "https-calls",
+                            networkTimeoutSeconds: 15,
+                            expiration: {
+                                maxEntries: 150,
+                                maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200]
+                            }
+                        }
+                    }
+                ],
+                importScripts: ["/static/sw-push-listener.js"]
             },
-        }],
-        importScripts: ['/static/sw-push-listener.js']
-    },
-    webpack: (config) => {
-        // this will output your push listener file to .next folder
-        // check CopyWebpackPlugin docs if you want to change the destination (e.g. /static or /.next/static)
-        config.plugins.push(new CopyWebpackPlugin([{from: 'static/sw-push-listener.js', src: '.next/sw-push-listener.js'}]));
-        config.node = {
-            fs: 'empty'
-        };
-        return config;
-    }
-}));
+            webpack: (config) => {
+                // this will output your push listener file to .next folder
+                // check CopyWebpackPlugin docs if you want to change the destination (e.g. /static or /.next/static)
+                config.plugins.push(
+                    new CopyWebpackPlugin([
+                        {
+                            from: "static/sw-push-listener.js",
+                            src: ".next/sw-push-listener.js"
+                        }
+                    ])
+                );
+                config.node = {
+                    fs: "empty"
+                };
+                return config;
+            }
+        })
+    )
+);
